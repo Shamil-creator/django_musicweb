17968b21387a4a0d5d5ea5ccb17e1a9a7bbcd866
Shamil-creator <67119390+Shamil-creator@users.noreply.github.com>
2020-08-21 14:08:18 +0300

    Update settings.py

--- musicweb/settings.py
+++ musicweb/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
